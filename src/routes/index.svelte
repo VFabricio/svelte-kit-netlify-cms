bc616566d8c5725c845456f30ccfd8d325ca319c
Vilson Fabricio Juliatto <fabricio@juliatto.com>
2021-06-30 19:02:02 -0300

    feat: add Netlify identity script to home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,6 @@
+<svelte:head>
+  <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
